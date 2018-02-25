5fb3399567e87b3f98119c68bb786e84cc08146d
Toponar Alexandr <toponar91@gmail.com>
2018-02-25 17:56:09 +0200

    добавил анимаций

--- inside windows/js/animate.js
+++ inside windows/js/animate.js
@@ -1,23 +1,35 @@
 jQuery(document).ready(function() {
-	jQuery('.benefits-slider').addClass("hidden").viewportChecker({
-		classToAdd: 'visible animated bounceInRight',
-		offset: 100
-	});
-	jQuery('.how-to-order').addClass("hidden").viewportChecker({
-		classToAdd: 'visible animated bounceInLeft',
-		offset: 100
-	});
+	
 	jQuery('.videolist').addClass("hidden").viewportChecker({
 		classToAdd: 'visible animated bounceInRight',
 		offset: 100
 	});
-	jQuery('.offer-body--left-section').addClass("hidden").viewportChecker({
-		classToAdd: 'visible animated bounceInUp',
-		offset: 100
-	});
 	jQuery('.offer-body--rignt-section').addClass("hidden").viewportChecker({
-		classToAdd: 'visible animated bounceInDown',
-		offset: 100
+		classToAdd: 'visible animated bounceInUp',
+		offset: 300
 	});
+	jQuery('.offer-body--left-section').addClass("hidden").viewportChecker({
+		classToAdd: 'visible animated bounceInDown',
+		offset: 30
+	});
+	jQuery('.catalogue').addClass("hidden").viewportChecker({
+		classToAdd: 'visible animated slideInLeft',
+		offset: 500
+	});	
+	jQuery('.popular-product').addClass("hidden").viewportChecker({
+		classToAdd: 'visible animated zoomIn',
+		offset: 300
+	});	
+	jQuery('.footer-conent').addClass("hidden").viewportChecker({
+		classToAdd: 'visible animated slideInUp',
+		offset: 300
+	});	
 });
+
+
+
+
+
+
+
 
