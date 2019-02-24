b5ccb813bdb86047eb0b69148023afc539acb201
Toponar Alexandr <toponar91@gmail.com>
2019-02-24 14:33:48 +0200

    added new project

---
+++ Vigonation_product/js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+  $("#capsule").twentytwenty({
+   
+    orientation: 'vertical', // Orientation of the before and after images ('horizontal' or 'vertical')
+   
+    no_overlay: true //Do not show the overlay with before and after   
+   
+  });
+});
---
+++ Vigonation_product/js/nicescroll.js
@@ -0,0 +1,9 @@
+$(".coments-block").niceScroll({
+ cursorcolor: "#000000",
+ cursorfixedheight: 34,
+ cursorwidth:"4px",
+ cursorborder: "none",
+ cursorborderradius: "none",
+ cursoropacitymin: 1
+});
+
